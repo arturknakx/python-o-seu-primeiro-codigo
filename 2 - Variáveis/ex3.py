ec21d2e8696e9ff4cbb12147fa498ac2b7d22c5a
arturknakx <162928543+arturknakx@users.noreply.github.com>
2024-10-03 13:30:53 -0300

    Add files via upload

---
+++ 2 - Variáveis/ex3.py
@@ -0,0 +1,5 @@
+nome = 'Artur'
+
+print(f'Meu nome é {nome}')
+print(f'Você acha {nome} um nome bonito?')
+print(f'Conheço outro {nome} também')
---
+++ 2 - Variáveis/ex5.py
@@ -0,0 +1,8 @@
+# Variável inteira:
+idade = 21
+
+# Variável float:
+salario = 8500.99
+
+# Variável boolean:
+cadastrado = True
