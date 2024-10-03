c43030ae8862ff69b808759a39d1a19d6fbe5e66
arturknakx <162928543+arturknakx@users.noreply.github.com>
2024-10-03 13:30:36 -0300

    Add files via upload

---
+++ 1 - Print e operações matemáticas/ex2.py
@@ -0,0 +1,20 @@
+# Adição:
+print(3 + 2)
+
+# Subtração:
+print(3 - 2)
+
+# Multiplicação:
+print(3 * 2)
+
+# Divisão:
+print(3 / 2)
+
+# Divisão inteira:
+print(3 // 2)
+
+# Módulo:
+print(3 % 2)
+
+# Potenciação:
+print(3 ** 2)
